36b25d9b003faccb1d92e043283bade935a2b056
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-21 14:13:01 -0500

    debugging codegrade tests

--- api/server.test.js
+++ api/server.test.js
@@ -45,80 +45,80 @@ describe("auth-router", () => {
       });
     }); //Happy path
 
-    //     describe("Sad path: Bad inputs", () => {
-    //       const { username, userNoName } = user1;
+    describe("Sad path: Bad inputs", () => {
+      const { username, userNoName } = user1;
 
-    //       describe("invalid username", () => {
-    //         // it("should respond with 400 on duplicate username", async () => {
-    //         //   await request(server).post(registerEp).send(user1Copy);
-    //         //   const res = await request(server).post(registerEp).send(user1Copy);
-    //         //   expect(res.status).toBe(400);
-    //         // });
+      describe("invalid username", () => {
+        // it("should respond with 400 on duplicate username", async () => {
+        //   await request(server).post(registerEp).send(user1Copy);
+        //   const res = await request(server).post(registerEp).send(user1Copy);
+        //   expect(res.status).toBe(400);
+        // });
 
-    //         it.todo(
-    //           "should respond with proper error on duplicate username"
-    //           // , async () => {
+        it.todo(
+          "should respond with proper error on duplicate username"
+          // , async () => {
 
-    //           // }
-    //         );
-    //         //duplicate username
+          // }
+        );
+        //duplicate username
 
-    //         it.todo(
-    //           "should respond with 400 on missing username"
-    //           //, async ()=>{
+        it.todo(
+          "should respond with 400 on missing username"
+          //, async ()=>{
 
-    //           //}
-    //         );
-    //         it.todo(
-    //           "should respond with proper error on missing username"
-    //           //, async ()=>{
+          //}
+        );
+        it.todo(
+          "should respond with proper error on missing username"
+          //, async ()=>{
 
-    //           //}
-    //         );
+          //}
+        );
 
-    //         it.todo(
-    //           "should respond with 400 on invalid username"
-    //           //, async ()=>{
+        it.todo(
+          "should respond with 400 on invalid username"
+          //, async ()=>{
 
-    //           //}
-    //         );
+          //}
+        );
 
-    //         it.todo(
-    //           "should respond with proper on invalid username"
-    //           //, async ()=>{
+        it.todo(
+          "should respond with proper error on invalid username"
+          //, async ()=>{
 
-    //           //}
-    //         );
-    //       }); //invalid username
+          //}
+        );
+      }); //invalid username
 
-    //       describe("invalid password", () => {
-    //         it.todo(
-    //           "should respond with 400 on missing password"
-    //           //, async ()=>{
+      //       describe("invalid password", () => {
+      //         it.todo(
+      //           "should respond with 400 on missing password"
+      //           //, async ()=>{
 
-    //           //}
-    //         );
-    //         it.todo(
-    //           "should respond with proper error on missing password"
-    //           //, async ()=>{
+      //           //}
+      //         );
+      //         it.todo(
+      //           "should respond with proper error on missing password"
+      //           //, async ()=>{
 
-    //           //}
-    //         );
+      //           //}
+      //         );
 
-    //         it.todo(
-    //           "should respond with 400 on invalid password"
-    //           //, async ()=>{
+      //         it.todo(
+      //           "should respond with 400 on invalid password"
+      //           //, async ()=>{
 
-    //           //}
-    //         );
+      //           //}
+      //         );
 
-    //         it.todo(
-    //           "should respond with proper on invalid password"
-    //           //, async ()=>{
+      //         it.todo(
+      //           "should respond with proper on invalid password"
+      //           //, async ()=>{
 
-    //           //}
-    //         );
-    //       }); //invalid password
-    //     }); //Sad path STATUS 400
+      //           //}
+      //         );
+      //       }); //invalid password
+    }); //Sad path STATUS 400
   }); //[POST] /registration
 }); //auth-router
